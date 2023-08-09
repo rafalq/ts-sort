b4b872aa8a1314a213c734b457f3ca402dbc6b6e
rafalq <kruszewskirafael@gmail.com>
2023-08-09 11:12:15 +0200

    create class for sort

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// class
+
+class Sorter {
+	// collection: number[];
+	// constructor(collection: number[]) {
+	//   this.collection = collection;
+	// }
+
+	constructor(public collection: number[]) {}
+
+	sort(): void {}
+}
