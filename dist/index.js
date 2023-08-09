f1dc946f8d66da485ea4a4367376c560e4f94848
rafalq <kruszewskirafael@gmail.com>
2023-08-09 11:27:10 +0200

    add sort method to the Sorter class

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log("hello, sir");
+// class
+class Sorter {
+    // collection: number[];
+    // constructor(collection: number[]) {
+    //   this.collection = collection;
+    // }
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] >
+                    this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] =
+                        this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+const sort = new Sorter([42, -100, 5, 0, 10, -1]);
+sort.sort();
+console.log(sort.collection);
--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,26 @@ class Sorter {
 
 	constructor(public collection: number[]) {}
 
-	sort(): void {}
+	sort(): void {
+		const { length } = this.collection;
+
+		for (let i = 0; i < length; i++) {
+			for (let j = 0; j < length - i - 1; j++) {
+				if (
+					this.collection[j] >
+					this.collection[j + 1]
+				) {
+					const leftHand = this.collection[j];
+					this.collection[j] =
+						this.collection[j + 1];
+					this.collection[j + 1] = leftHand;
+				}
+			}
+		}
+	}
 }
+
+const sort = new Sorter([42, -100, 5, 0, 10, -1]);
+
+sort.sort();
+console.log(sort.collection);
