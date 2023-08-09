19fb12eff3ba1f386fe3c82f808c51250aec82b5
rafalq <kruszewskirafael@gmail.com>
2023-08-09 11:33:58 +0200

    create function for sorting

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,56 @@
-// class
+// function
 
-class Sorter {
-	// collection: number[];
-	// constructor(collection: number[]) {
-	//   this.collection = collection;
-	// }
-
-	constructor(public collection: number[]) {}
-
-	sort(): void {
-		const { length } = this.collection;
-
-		for (let i = 0; i < length; i++) {
-			for (let j = 0; j < length - i - 1; j++) {
-				if (
-					this.collection[j] >
-					this.collection[j + 1]
-				) {
-					const leftHand = this.collection[j];
-					this.collection[j] =
-						this.collection[j + 1];
-					this.collection[j + 1] = leftHand;
-				}
+function sortItems(
+	collection: number[]
+): number[] {
+	for (let i = 0; i < collection.length; i++) {
+		for (
+			let j = 0;
+			j < collection.length - i - 1;
+			j++
+		) {
+			if (collection[j] > collection[j + 1]) {
+				const leftHand = collection[j];
+				collection[j] = collection[j + 1];
+				collection[j + 1] = leftHand;
 			}
 		}
 	}
+	return collection;
 }
 
-const sort = new Sorter([42, -100, 5, 0, 10, -1]);
+console.log(sortItems([123, 0, -7, 2.56, 20]));
 
-sort.sort();
-console.log(sort.collection);
+// // class
+
+// class Sorter {
+// 	// collection: number[];
+// 	// constructor(collection: number[]) {
+// 	//   this.collection = collection;
+// 	// }
+
+// 	constructor(public collection: number[]) {}
+
+// 	sort(): void {
+// 		const { length } = this.collection;
+
+// 		for (let i = 0; i < length; i++) {
+// 			for (let j = 0; j < length - i - 1; j++) {
+// 				if (
+// 					this.collection[j] >
+// 					this.collection[j + 1]
+// 				) {
+// 					const leftHand = this.collection[j];
+// 					this.collection[j] =
+// 						this.collection[j + 1];
+// 					this.collection[j + 1] = leftHand;
+// 				}
+// 			}
+// 		}
+// 	}
+// }
+
+// const sort = new Sorter([42, -100, 5, 0, 10, -1]);
+
+// sort.sort();
+// console.log(sort.collection);
